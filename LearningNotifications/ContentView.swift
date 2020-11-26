614eb60757ddaf8b278bfaaef10d5bc224079c6d
Tejas M R <matblockgreninja@gmail.com>
2020-11-26 19:58:19 +0530

    Done

--- LearningNotifications/ContentView.swift
+++ LearningNotifications/ContentView.swift
@@ -6,12 +6,70 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    
+    var daysDict = [
+        "Sun" : 1,
+        "Mon" : 2,
+        "Tue" : 3,
+        "Wed" : 4,
+        "Thu" : 5,
+        "Fri" : 6,
+        "Sat" : 7
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Button(action: {
+                setNotification()
+            }) {
+                Text("Click")
+                    .padding()
+            }
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(color: Color.black, radius: 3, x: 0, y: 0)
+        }
+        
     }
+    
+    
+    func setNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                print("All set!")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "The title"
+        content.body = "The content"
+        
+        let date = Date().addingTimeInterval(5)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .weekday, .weekdayOrdinal, .timeZone, .hour, .minute, .second], from: date)
+        
+        print(Calendar.current.dateComponents([.year, .month, .day, .weekday, .weekdayOrdinal, .timeZone, .hour, .minute, .second], from: Date()))
+        print(dateComponents)
+        
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { (error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "Error without description")
+                print(error.debugDescription)
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +77,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- LearningNotifications/LearningNotificationsApp.swift
+++ LearningNotifications/LearningNotificationsApp.swift
@@ -7,8 +7,29 @@
 
 import SwiftUI
 
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+        -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+}
+
 @main
 struct LearningNotificationsApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
