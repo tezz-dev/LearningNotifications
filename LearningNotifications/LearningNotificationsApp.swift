1cb8be27fa7c371e29b31863e7d2f2b05ffa5c32
Tejas M R <matblockgreninja@gmail.com>
2020-11-26 16:45:54 +0530

    Initial Commit

---
+++ LearningNotifications/LearningNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningNotificationsApp.swift
+//  LearningNotifications
+//
+//  Created by tezz on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LearningNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
